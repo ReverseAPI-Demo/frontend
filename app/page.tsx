cdc49d3222e21d147437a1f04c84ff9f40cb793d
neelthepatel8 <patel.neel5@northeastern.edu>
2025-03-15 15:12:56 -0400

    add loading delay for better exp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MainSidebar } from "@/components/main-sidebar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -17,6 +17,15 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function HomePage() {
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <SidebarProvider>
             <MainSidebar />
@@ -36,14 +45,18 @@ export default function HomePage() {
                         </BreadcrumbList>
                     </Breadcrumb>
                 </header>
-                <div className="flex flex-1 flex-col gap-4 p-4">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <Skeleton className="aspect-video" />
-                        <Skeleton className="aspect-video" />
-                        <Skeleton className="aspect-video" />
+                {isLoading ? (
+                    <div className="flex flex-1 flex-col gap-4 p-4">
+                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                            <Skeleton className="aspect-video" />
+                            <Skeleton className="aspect-video" />
+                            <Skeleton className="aspect-video" />
+                        </div>
+                        <Skeleton className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
                     </div>
-                    <Skeleton className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-                </div>
+                ) : (
+                    <div></div>
+                )}
             </SidebarInset>
         </SidebarProvider>
     );
